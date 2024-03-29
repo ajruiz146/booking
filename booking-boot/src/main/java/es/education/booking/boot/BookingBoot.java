6a661a74f26de345de0cf48d835c691e5998988f
Antonio José <ajruiz146@gmail.com>
2024-03-29 20:01:12 +0100

    pom - packaging

--- booking-boot/src/main/java/es/education/booking/boot/BookingBoot.java
+++ booking-boot/src/main/java/es/education/booking/boot/BookingBoot.java
@@ -2,7 +2,10 @@ package es.education.booking.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
+@ComponentScan(basePackages = "es.education.booking")
 @SpringBootApplication
 public class BookingBoot {
 
